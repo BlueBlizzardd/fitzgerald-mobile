f0018db1625311b9c721f03b474bad481cce5d35
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2025-03-17 15:08:43 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { createSignal } from 'solid-js'
+import './App.css'
+
+function App() {
+    return (
+        <></>
+    )
+}
+
+export default App
---
+++ src/components/featured/featured.tsx
@@ -0,0 +1,12 @@
+import { Component } from "solid-js";
+
+type FeaturedProps = {
+    image: string;
+    title: string;
+    description: string;
+}
+
+export const Featured: Component<FeaturedProps> = (props) => {
+    return <>
+    </>
+}
